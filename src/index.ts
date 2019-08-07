36930f7350aa27bd81de5fa5d59a5f6f6701f9a6
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-08-07 22:26:01 +0200

    refactor getting artifacts

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,16 @@
 import { codechecks } from "@codechecks/client";
-import * as glob from "glob";
 
-import { BuildSizeWatcherOptions, FileArtifact, FullArtifact } from "./types";
-import { getSize } from "./getSize";
-import { join } from "path";
+import { BuildSizeWatcherOptions, FullArtifact } from "./types";
 import { getArtifactDiff } from "./getArtifactDiff";
 import { getReportFromDiff } from "./getReportFromDiff";
 import { normalizeOptions } from "./normalization";
+import { getArtifact } from "./getArtifact";
 
 export async function buildSizeWatcher(_options: BuildSizeWatcherOptions): Promise<void> {
   const options = normalizeOptions(_options);
   const cwd = codechecks.context.workspaceRoot;
 
-  const fullArtifact: FullArtifact = {};
-
-  for (const file of options.files) {
-    const matches = glob.sync(file.path, { cwd });
-
-    const sizes = await Promise.all(matches.map(match => getSize(join(cwd, match), options.gzip)));
-    const overallSize = sizes.reduce((a, b) => a + b, 0);
-
-    const artifact: FileArtifact = {
-      path: file.path,
-      files: matches.length,
-      overallSize,
-    };
-
-    fullArtifact[file.path] = artifact;
-  }
+  const fullArtifact = await getArtifact(options, cwd);
 
   await codechecks.saveValue(options.artifactName, fullArtifact);
 
